f226a7d0da8825d352bb2f9f966a6739493c9568
Cesar Lopes <devcesarlopes@gmail.com>
2025-04-22 21:06:15 -0300

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,84 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+	{ ignores: ['dist'] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			import: importPlugin,
+		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					project: './tsconfig.json',
+				},
+			},
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			quotes: ['warn', 'single'],
+			semi: ['warn', 'always'],
+			'jsx-quotes': ['error', 'prefer-single'],
+			'@typescript-eslint/no-unused-vars': 'off',
+			'import/no-unresolved': 'error',
+			'import/order': [
+				'warn',
+				{
+					groups: [
+						'builtin', // ex: fs, path, react, react-dom
+						'external', // ex: lodash, axios
+						'internal', // ex: @/*
+						'parent', // ex: ../algo
+						'sibling', // ex: ./algo
+						'index', // ex: './'
+					],
+					pathGroups: [
+						{
+							pattern: '{react,vite}',
+							group: 'builtin',
+							position: 'before',
+						},
+						{
+							pattern: '@/assets/**',
+							group: 'internal',
+							position: 'after',
+						},
+						{
+							pattern: '@/components/**',
+							group: 'internal',
+							position: 'after',
+						},
+						{
+							pattern: '@/**',
+							group: 'internal',
+							position: 'after',
+						},
+					],
+					pathGroupsExcludedImportTypes: ['react'],
+					distinctGroup: true,
+					'newlines-between': 'always',
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+		},
+	},
+);
---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+import { twMerge } from 'tailwind-merge';
+
+import reactLogo from '@/assets/react.svg';
+
+import { MyComponent } from '@/components/MyComponent';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div>
+				<a href='https://vite.dev' target='_blank'>
+					<img src={'/vite.svg'} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://react.dev' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button
+					className={twMerge(
+						'bg-orange-400 p-3 text-white',
+						count && 'bg-red-600',
+						count > 10 && 'bg-blue-900',
+					)}
+					onClick={() => setCount((count) => count + 1)}
+				>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='bg-slate-900 text-3xl font-bold underline'>
+				Click on the Vite and React logos to learn more
+			</p>
+			<p className='bg-gray-100 text-3xl font-bold underline'>
+				Click on the Vite and React logos to learn more
+			</p>
+			<MyComponent />
+		</>
+	);
+}
+
+export default App;
